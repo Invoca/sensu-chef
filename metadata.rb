5e52fc0c88081f847eccfe3fe9ac1bc974748a40
Bob Smith <bob@invoca.com>
2014-03-11 18:19:15 -0700

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,9 @@ maintainer_email "chefs@sonian.net"
 license          "Apache 2.0"
 description      "Installs/Configures Sensu"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.8.0"
+
+# Added 1.0.0 to public version
+version          "1.8.1"
 
 # available @ http://community.opscode.com/cookbooks/apt
 depends "apt"
